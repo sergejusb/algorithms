df4a61253b9a58fb8608060ba0c1a7d6f5e934fb
Sergejus <seriog@gmail.com>
2013-06-02 22:32:21 +0300

    implemented add method

--- linkedList.js
+++ linkedList.js
@@ -13,34 +13,53 @@ var SinglyLinkedList = (function () {
         this.head = new Node(undefined, null);
     }
 
-    SinglyLinkedList.prototype.length = function() {
-        var length = 0;
-        var current = this.head.next;
-
-        while(current !== null) {
-            length++;
-            current = current.next;
-        }
-
-        return length;
-    }
-
     SinglyLinkedList.prototype.toArray = function() {
         var array = [];
-        var current = this.head.next;
+        var next = this.head.next;
 
-        while(current !== null) {
-            array.push(current.item);
-            current = current.next;
+        while(next !== null) {
+            array.push(next.item);
+            next = next.next;
         }
 
         return array;
     }
 
+    SinglyLinkedList.prototype.add = function(item, position) {
+        var current = this.head;
+        var next = current.next;
+        var index = 0;
+
+        if (position < 0) {
+            throw new Error("position must be positive number");
+        }
+
+        while (next !== null && index++ !== position) {
+            current = next;
+            next = next.next;
+        }
+
+        current.next = new Node(item, next);
+
+        return this;
+    }
+
+    SinglyLinkedList.prototype.length = function() {
+        var length = 0;
+        var next = this.head.next;
+
+        while(next !== null) {
+            length++;
+            next = next.next;
+        }
+
+        return length;
+    }
+
     SinglyLinkedList.prototype.reverse = function() {
         var previous = null;
         var current = this.head;
-        var next = this.head.next;
+        var next = current.next;
 
         while (next !== null) {
             previous = current;
@@ -60,11 +79,11 @@ var SinglyLinkedList = (function () {
     SinglyLinkedList.prototype.copy = function() {
         var list = new SinglyLinkedList();
         var copy = list.head;
-        var current = this.head.next;
+        var next = this.head.next;
 
-        while (current !== null) {
-            copy.next = new Node(current.item, null);
-            current = current.next;
+        while (next !== null) {
+            copy.next = new Node(next.item, null);
+            next = next.next;
             copy = copy.next;
         }
 
@@ -120,28 +139,6 @@ var LinkedListTestRunner = (function () {
                 list = stackFactory();
             });
 
-            describe("length()", function() {
-                it("is 0 for newly created list", function() {
-                    assert.equal(0, list.length());
-                });
-
-                it("is 1 when contains 1 item", function() {
-                    list.head = new Node(undefined, new Node(1, null));
-                    assert.equal(1, list.length());
-                });
-
-                it("is 3 when contains 3 items", function() {
-                    list.head = new Node(undefined, new Node(1, new Node(2, new Node(3, null))));
-                    assert.equal(3, list.length());
-                });
-
-                it("can call multiple times", function() {
-                    list.head = new Node(undefined, new Node(1, null));
-                    list.length();
-                    assert.equal(1, list.length());
-                });
-            });
-
             describe("toArray()", function() {
                 it("is [] when empty list", function() {
                     assertMany([], list.toArray());
@@ -158,18 +155,74 @@ var LinkedListTestRunner = (function () {
                 });            
             });
 
+            describe("add()", function() {
+                it("is <1> when 1 added to empty list at position 0", function() {
+                    list.add(1, 0);
+                    assertMany([1], list.toArray());
+                });
+
+                it("is <1> when 1 added to empty list at position 1", function() {
+                    list.add(1, 1);
+                    assertMany([1], list.toArray());
+                });
+
+                it("is <2,1> when 2 added to <1> at position 0", function() {
+                    list.add(1, 0);
+                    list.add(2, 0);
+                    assertMany([2,1], list.toArray());
+                });
+
+                it("is <1,2> when 2 added to <1> at position 1", function() {
+                    list.add(1, 0);
+                    list.add(2, 1);
+                    assertMany([1,2], list.toArray());
+                });
+
+                it("throws when position is negative", function() {
+                    assert.throws(function() {
+                        list.add(1, -1);
+                    });
+                });
+            });
+            
+            describe("length()", function() {
+                it("is 0 for newly created list", function() {
+                    assert.equal(0, list.length());
+                });
+
+                it("is 1 when contains 1 item", function() {
+                    list.add(1, 0);
+                    assert.equal(1, list.length());
+                });
+
+                it("is 3 when contains 3 items", function() {
+                    list.add(1,0);
+                    list.add(2,1);
+                    list.add(3,2);
+                    assert.equal(3, list.length());
+                });
+
+                it("can call multiple times", function() {
+                    list.add(1,0);
+                    list.length();
+                    assert.equal(1, list.length());
+                });
+            });
+
             describe("reverse()", function() {
                 it("is <> when empty list", function() {
                     assertMany([], list.reverse().toArray());
                 });
 
                 it("is <1> when <1>", function() {
-                    list.head = new Node(undefined, new Node(1, null));
+                    list.add(1,0);
                     assertMany([1], list.reverse().toArray());
                 });
 
                 it("is <3,2,1> when <1,2,3>", function() {
-                    list.head = new Node(undefined, new Node(1, new Node(2, new Node(3, null))));
+                    list.add(1,0);
+                    list.add(2,1);
+                    list.add(3,2);
                     assertMany([3,2,1], list.reverse().toArray());
                 });
             });
@@ -180,12 +233,14 @@ var LinkedListTestRunner = (function () {
                 });
 
                 it("is <1> when <1>", function() {
-                    list.head = new Node(undefined, new Node(1, null));
+                    list.add(1,0);
                     assertMany([1], list.copy().toArray());
                 });
 
                 it("is <1,2,3> when <1,2,3>", function() {
-                    list.head = new Node(undefined, new Node(1, new Node(2, new Node(3, null))));
+                    list.add(1,0);
+                    list.add(2,1);
+                    list.add(3,2);
                     assertMany([1,2,3], list.copy().toArray());
                 });
             });
@@ -201,30 +256,35 @@ var LinkedListTestRunner = (function () {
                 });
 
                 it("is <1> when empty list merged with <1>", function() {
-                    anotherList.head = new Node(undefined, new Node(1, null));
+                    anotherList.add(1,0);
                     assertMany([1], list.merge(anotherList).toArray());
                 });
 
                 it("is <1> when <1> merged with empty list", function() {
-                    list.head = new Node(undefined, new Node(1, null));
+                    list.add(1,0);
                     assertMany([1], list.merge(anotherList).toArray());
                 });
 
                 it("is <1,2> when <1> merged with <2>", function() {
-                    list.head = new Node(undefined, new Node(1, null));
-                    anotherList.head = new Node(undefined, new Node(2, null));
+                    list.add(1,0);
+                    anotherList.add(2,0);
                     assertMany([1,2], list.merge(anotherList).toArray());
                 });
 
                 it("is <1,2,3,4> when <1,2> merged with <3,4>", function() {
-                    list.head = new Node(undefined, new Node(1, new Node(2, null)));
-                    anotherList.head = new Node(undefined, new Node(3, new Node(4, null)));
+                    list.add(1,0);
+                    list.add(2,1);
+                    anotherList.add(3,0);
+                    anotherList.add(4,1);
                     assertMany([1,2,3,4], list.merge(anotherList).toArray());
                 });
 
                 it("is <1,2,3,4,5> when <1,2,3> merged with <4,5>", function() {
-                    list.head = new Node(undefined, new Node(1, new Node(2, new Node(3, null))));
-                    anotherList.head = new Node(undefined, new Node(4, new Node(5, null)));
+                    list.add(1,0);
+                    list.add(2,1);
+                    list.add(3,2);
+                    anotherList.add(4,0);
+                    anotherList.add(5,1);
                     assertMany([1,2,3,4,5], list.merge(anotherList).toArray());
                 });
             });
