de15bf97daae284dfc3a760df3508c2b6848c1c7
Sergejus <seriog@gmail.com>
2013-06-26 17:28:03 +0300

    selection sort

---
+++ sorting/selectionSort.js
@@ -0,0 +1,17 @@
+function selectionSort(numbers) {
+    for (var i = 0; i < numbers.length; i++) {
+        var min = i;
+        for (var n = i + 1; n < numbers.length; n++) {
+            if (numbers[n] < numbers[min]) {
+                min = n;
+            }
+        };
+        var number = numbers[i];
+        numbers[i] = numbers[min];
+        numbers[min] = number;
+    };
+
+    return numbers;
+}
+
+module.exports = selectionSort;
---
+++ sorting/sort_tests.js
@@ -0,0 +1,46 @@
+var selectionSort = require('./selectionSort.js');
+var insertionSort = require('./insertionSort.js');
+
+var SortTestRunner = (function() {
+    function SortTestRunner() {
+    }
+
+    SortTestRunner.prototype.runTests = function(sortFunction) {
+        var assert = require("assert");
+
+        var assertMany = function(expected, actual) {
+            for (var i = 0; i < actual.length; i++) {
+                assert.equal(expected[i], actual[i]);
+             };
+
+            assert.equal(expected.length, actual.length);
+        }
+
+        describe(sortFunction.name + "()", function() {
+            it("can sort empty array", function() {
+                assertMany([], sortFunction([]));
+            });
+
+            it("can sort single number", function() {
+                assertMany([1], sortFunction([1]));
+            });
+
+            it("can sort unique numbers", function() {
+                assertMany([1,2,3,4,5], sortFunction([4,1,3,5,2]));
+            });
+
+            it("can sort non-unique numbers", function() {
+                assertMany([1,1,2,3,3,4,5], sortFunction([3,4,1,3,5,1,2]));
+            });
+
+            it("can sort negative numbers", function() {
+                assertMany([-2,-1,0,0,1,2], sortFunction([1,0,-2,2,-1,0]));
+            });
+        });
+    }
+
+    return SortTestRunner;
+})();
+
+var testRunner = new SortTestRunner();
+testRunner.runTests(selectionSort);
