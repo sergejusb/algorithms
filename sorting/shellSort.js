454e9f722b5f0e70b4431b193666fa468bcca7d3
Sergejus <seriog@gmail.com>
2013-08-19 22:34:41 +0300

    updated shellsort

--- sorting/shellSort.js
+++ sorting/shellSort.js
@@ -4,14 +4,15 @@ function shellSort(numbers) {
         h = 3 * h + 1;
     }
 
-    for (; h >= 1; h = --h / 3) {
-        for (var i = 0; i < numbers.length; i += h) {
+    while (h > 0) {
+        for (var i = h; i < numbers.length; i += h) {
             for (var n = i; n > 0 && numbers[n] < numbers[n-h]; n -= h) {
                 var number = numbers[n];
                 numbers[n] = numbers[n-h];
                 numbers[n-h] = number;
             }
         }
+        h = --h / 3;
     }
 
     return numbers;
