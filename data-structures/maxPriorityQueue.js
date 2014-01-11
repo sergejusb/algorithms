7496c5f9655b79869f026c8a8dbfd49f8a20c1c9
Sergejus <seriog@gmail.com>
2014-01-11 12:43:17 +0200

    added maxPriorityQueue

---
+++ data-structures/maxPriorityQueue.js
@@ -0,0 +1,74 @@
+var Key = (function() {
+    function Key(value, priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+
+    return Key;
+})();
+
+var MaxPriorityQueue = (function() {
+    function MaxPriorityQueue() {
+        this.items = [null];
+        this.count = 0;
+    }
+
+    MaxPriorityQueue.prototype.insert = function(value, priority) {
+        this.items.push(new Key(value, priority));
+        this.count++;
+        
+        /* ensure binary heap invariant */
+        var child = this.count;
+        var parent = child >> 1;
+        while (child > 1 && this.items[child].priority > this.items[parent].priority) {
+            var tmp = this.items[child];
+            this.items[child] = this.items[parent];
+            this.items[parent] = tmp;
+            
+            child = parent;
+            parent = child >> 1;
+        }
+    };
+
+    MaxPriorityQueue.prototype.findMax  = function() {
+        if (this.count === 0) throw "Priority queue is empty";
+
+        return this.items[1].value;
+    };
+
+    MaxPriorityQueue.prototype.deleteMax = function() {
+        if (this.count === 0) throw "Priority queue is empty";
+
+        var item = this.items[1];
+        this.items[1] = this.items.pop();
+        this.count--;
+
+        /* ensure binary heap invariant */
+        var parent = 1;
+        var child = parent << 1;
+        while (child <= this.count) {
+            if (child < this.count) {
+                child = this.items[child].priority > this.items[child + 1].priority ? child : child + 1;
+            }
+            
+            if (this.items[parent].priority > this.items[child].priority) break;
+
+            var tmp = this.items[parent];
+            this.items[parent] = this.items[child];
+            this.items[child] = tmp;
+
+            parent = child;
+            child = parent << 1;
+        }
+
+        return item.value;
+    };
+
+    MaxPriorityQueue.prototype.isEmpty = function() {
+        return this.count === 0;
+    };
+
+    return MaxPriorityQueue;
+})();
+
+module.exports = MaxPriorityQueue;
---
+++ data-structures/maxPriorityQueue_tests.js
@@ -0,0 +1,119 @@
+var BinaryHeap = require('./binaryHeap.js');
+var MaxPriorityQueue = require('./maxPriorityQueue.js');
+
+var MaxPriorityQueueTestRunner = (function () {
+    function MaxPriorityQueueTestRunner() {
+    }
+
+    MaxPriorityQueueTestRunner.prototype.runTests = function() {
+        var assert = require("assert");
+        
+        describe("Priority queue (max)", function() {
+            var maxPriorityQueue = null;
+
+            beforeEach(function() {
+                maxPriorityQueue = new MaxPriorityQueue();
+            });
+
+
+            describe("insert()", function() {
+                it("is 'a' when 'a' inserted with priority 0 into empty priority queue", function() {
+                    maxPriorityQueue.insert('a', 0);
+                    
+                    assert.equal('a', maxPriorityQueue.findMax());
+                });
+
+                it("is 'b' when 'a' inserted with priority 0 and 'b' inserted with priority 1", function() {
+                    maxPriorityQueue.insert('a', 0);
+                    maxPriorityQueue.insert('b', 1);
+                    
+                    assert.equal('b', maxPriorityQueue.findMax());
+                });
+            });
+
+            describe("deleteMax()", function() {
+                it("throws when empty", function() {
+                    assert.throws(function() {
+                        maxPriorityQueue.deleteMax();
+                    });
+                });
+
+                it("returns 'a' when 'a' inserted with priority 0 into empty priority queue", function() {
+                    maxPriorityQueue.insert('a', 0);
+                    
+                    assert.equal('a', maxPriorityQueue.deleteMax());
+                });
+
+                it("returns 'b' when 'a' inserted with priority 0 and 'b' inserted with priority 1", function() {
+                    maxPriorityQueue.insert('a', 0);
+                    maxPriorityQueue.insert('b', 1);
+                    
+                    assert.equal('b', maxPriorityQueue.deleteMax());
+                });
+
+                it("returns 'b' when 'a' and 'c' inserted with priority 0 and 'b' inserted with priority 1", function() {
+                    maxPriorityQueue.insert('a', 0);
+                    maxPriorityQueue.insert('b', 1);
+                    maxPriorityQueue.insert('c', 0);
+                    
+                    assert.equal('b', maxPriorityQueue.deleteMax());
+                });
+
+                it("returns 'a', 'b' and 'c' when inserted with priority 2, 1 and 0 respectively", function() {
+                    maxPriorityQueue.insert('a', 2);
+                    maxPriorityQueue.insert('b', 1);
+                    maxPriorityQueue.insert('c', 0);
+                    
+                    assert.equal('a', maxPriorityQueue.deleteMax());
+                    assert.equal('b', maxPriorityQueue.deleteMax());
+                    assert.equal('c', maxPriorityQueue.deleteMax());
+                });
+            });
+
+            describe("findMax()", function() {
+                it("throws when empty", function() {
+                    assert.throws(function() {
+                        maxPriorityQueue.findMax();
+                    });
+                });
+
+                it("returns 'a' when 'a' inserted with priority 0 into empty priority queue", function() {
+                    maxPriorityQueue.insert('a', 0);
+                    
+                    assert.equal('a', maxPriorityQueue.findMax());
+                });
+
+                it("returns, but not removes item from priority queue", function() {
+                    maxPriorityQueue.insert('a', 0);
+                    maxPriorityQueue.findMax();
+
+                    assert.equal(false, maxPriorityQueue.isEmpty());
+                });
+            });
+
+            describe("isEmpty()", function() {
+                it("is true for newly created priority queue", function() {
+                    assert.equal(true, maxPriorityQueue.isEmpty());
+                });
+
+                it("is false when single item inserted into empty priority queue", function() {
+                    maxPriorityQueue.insert('a', 0);
+
+                    assert.equal(false, maxPriorityQueue.isEmpty());
+                });
+
+                it("is true when single item inserted into empty priority queue and then removed", function() {
+                    maxPriorityQueue.insert('a', 0);
+                    maxPriorityQueue.deleteMax();
+
+                    assert.equal(true, maxPriorityQueue.isEmpty());
+                });
+            });
+        });
+    };
+
+    return MaxPriorityQueueTestRunner;
+})();
+
+var testRunner = new MaxPriorityQueueTestRunner();
+testRunner.runTests();
