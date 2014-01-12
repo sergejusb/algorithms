387a246a1f61bd8f31d27630985837985dd245e7
Sergejus <seriog@gmail.com>
2014-01-12 15:52:46 +0200

    added binarySearchTree

---
+++ data-structures/binarySearchTree.js
@@ -0,0 +1,191 @@
+var TreeNode = (function() {
+    function TreeNode(key, value) {
+        this.key = key;
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    return TreeNode;
+})();
+
+var BinarySearchTree = (function() {
+    function BinarySearchTree() {
+        this.root = null;
+    }
+
+    BinarySearchTree.prototype.search = function(key) {
+        var node = this.root;
+        while (node !== null) {
+            if (key < node.key) {
+                node = node.left;
+            }
+            else if (key > node.key) {
+                node = node.right;
+            }
+            else {
+                return node.value;
+            }
+        }
+
+        return null;
+    };
+
+    BinarySearchTree.prototype.insert = function(key, value, node) {
+        if (node === undefined) {
+            this.root = this.insert(key, value, this.root);
+            return;
+        }
+        
+        if (node === null) {
+            return new TreeNode(key, value);
+        }
+        
+        if (key < node.key) {
+            node.left = this.insert(key, value, node.left);
+        }
+        else if (key > node.key) {
+            node.right = this.insert(key, value, node.right);
+        }
+        else {
+            node.value = value;
+        }
+
+        return node;
+    };
+
+    BinarySearchTree.prototype.delete = function(key, node) {
+        if (node === undefined) {
+            this.root = this.delete(key, this.root);
+            return;
+        }
+
+        if (node === null) {
+            return null;
+        }
+
+        if (key < node.key) {
+            node.left = this.delete(key, node.left);
+        }
+        else if (key > node.key) {
+            node.right = this.delete(key, node.right);
+        }
+        else {
+            if (node.left === null) {
+                return node.right;
+            }
+            else if (node.right === null) {
+                return node.left;
+            }
+            else {
+                var tmp = node;
+                node = this.min(node.right);
+                node.right = this.deleteMin(tmp.right);
+                node.left = tmp.left;
+            }
+        }
+
+        return node;
+    };
+
+    BinarySearchTree.prototype.min = function(current) {
+        if (current === undefined) {
+            current = this.root;
+        }
+
+        if (current === null) {
+            return null;
+        }
+
+        while (current.left !== null) {
+            current = current.left;
+        }
+
+        return current;
+    };
+
+    BinarySearchTree.prototype.deleteMin = function(current) {
+        if (current === undefined) {
+            this.root = this.deleteMin(this.root);
+        }
+
+        if (current === null) {
+            return null;
+        }
+
+        if (current.left === null) {
+            return current.right;
+        }
+
+        current.left = this.deleteMin(current.left);
+
+        return current;
+    };
+
+    BinarySearchTree.prototype.traversePreOrder = function(nodes, current) {
+        if (nodes === undefined && current === undefined) {
+            return this.traversePreOrder([], this.root);
+        }
+
+        if (current !== null) {
+            nodes.push(current.value);
+            this.traversePreOrder(nodes, current.left);
+            this.traversePreOrder(nodes, current.right);
+        }
+
+        return nodes;
+    };
+
+    BinarySearchTree.prototype.traverseInOrder = function(nodes, current) {
+        if (nodes === undefined && current === undefined) {
+            return this.traverseInOrder([], this.root);
+        }
+
+        if (current !== null) {
+            this.traverseInOrder(nodes, current.left);
+            nodes.push(current.value);
+            this.traverseInOrder(nodes, current.right);
+        }
+
+        return nodes;
+    };
+
+    BinarySearchTree.prototype.traversePostOrder = function(nodes, current) {
+        if (nodes === undefined && current === undefined) {
+            return this.traversePostOrder([], this.root);
+        }
+
+        if (current !== null) {
+            this.traversePostOrder(nodes, current.left);
+            this.traversePostOrder(nodes, current.right);
+            nodes.push(current.value);
+        }
+
+        return nodes;
+    };
+
+    BinarySearchTree.prototype.traverseLevelOrder = function() {
+        var nodes = [];
+        var queue = [this.root];
+
+        while (queue.length > 0) {
+            var current = queue.shift();
+            if (current !== null) {
+                nodes.push(current.value);
+            }
+            if (current.left !== null) {
+                queue.push(current.left);
+            }
+            if (current.right !== null) {
+                queue.push(current.right);
+            }
+        }
+
+        return nodes;
+    };
+
+    return BinarySearchTree;
+})();
+
+module.exports = TreeNode;
+module.exports = BinarySearchTree;
---
+++ data-structures/binarySearchTree_tests.js
@@ -0,0 +1,192 @@
+var BinarySearchTree = require('./binarySearchTree.js');
+
+var BinarySearchTreeTestRunner = (function() {
+    function BinarySearchTreeTestRunner() {
+    }
+
+    BinarySearchTreeTestRunner.prototype.runTests = function() {
+        var assert = require("assert");
+        var assertMany = function(expected, actual) {
+            for (var i = 0; i < actual.length; i++) {
+                assert.equal(expected[i], actual[i]);
+             }
+
+            assert.equal(expected.length, actual.length);
+        };
+
+        var tree;
+        beforeEach(function() {
+            tree = new BinarySearchTree();
+        });
+
+        describe("Binary Search Tree", function() {
+            describe("search()", function() {
+                it("returns 'a' when binary search tree contains item {1,'a'}", function() {
+                    tree.insert(1, 'a');
+
+                    assert.equal('a', tree.search(1));
+                });
+
+                it("returns null when binary search tree does not contain item {1,'a'}", function() {
+                    tree.insert(2, 'b');
+
+                    assert.equal(null, tree.search(1));
+                });
+            });
+
+            describe("insert()", function() {
+                it("can insert {1,'a'} into empty binary search tree", function() {
+                    tree.insert(1, 'a');
+
+                    assert.equal('a', tree.search(1));
+                });
+                it("can insert {1,'b'} when {1,'a'} already exist", function() {
+                    tree.insert(1, 'a');
+                    tree.insert(1, 'b');
+
+                    assert.equal('b', tree.search(1));
+                });
+            });
+
+            describe("delete()", function() {
+                it("can delete leaves", function() {
+                    tree.insert(5, 5);
+                    tree.insert(2, 2);
+                    tree.insert(9, 9);
+                    tree.insert(1, 1);
+                    tree.insert(4, 4);
+                    tree.insert(7, 7);
+                    tree.insert(10, 10);
+                    tree.insert(3, 3);
+                    tree.insert(6, 6);
+                    tree.insert(8 ,8);
+
+                    tree.delete(1);
+                    tree.delete(10);
+
+                    assertMany([5,2,9,4,7,3,6,8], tree.traverseLevelOrder());
+                });
+
+                it("can delete node with single child", function() {
+                    tree.insert(5, 5);
+                    tree.insert(2, 2);
+                    tree.insert(9, 9);
+                    tree.insert(1, 1);
+                    tree.insert(4, 4);
+                    tree.insert(7, 7);
+                    tree.insert(10, 10);
+                    tree.insert(3, 3);
+                    tree.insert(6, 6);
+                    tree.insert(8 ,8);
+
+                    tree.delete(4);
+
+                    assertMany([5,2,9,1,3,7,10,6,8], tree.traverseLevelOrder());
+                });
+
+                it("can delete node with two children", function() {
+                    tree.insert(5, 5);
+                    tree.insert(2, 2);
+                    tree.insert(9, 9);
+                    tree.insert(1, 1);
+                    tree.insert(4, 4);
+                    tree.insert(7, 7);
+                    tree.insert(10, 10);
+                    tree.insert(3, 3);
+                    tree.insert(6, 6);
+                    tree.insert(8 ,8);
+
+                    tree.delete(7);
+
+                    assertMany([5,2,9,1,4,8,10,3,6], tree.traverseLevelOrder());
+                });
+
+                it("can delete root", function() {
+                    tree.insert(5, 5);
+                    tree.insert(2, 2);
+                    tree.insert(9, 9);
+                    tree.insert(1, 1);
+                    tree.insert(4, 4);
+                    tree.insert(7, 7);
+                    tree.insert(10, 10);
+                    tree.insert(3, 3);
+                    tree.insert(6, 6);
+                    tree.insert(8 ,8);
+
+                    tree.delete(5);
+
+                    assertMany([6,2,9,1,4,7,10,3,8], tree.traverseLevelOrder());
+                });
+            });
+
+            describe("traversePreOrder()", function() {
+                it("can correctly traverse binary search tree pre-order", function() {
+                    tree.insert('f', 'f');
+                    tree.insert('b', 'b');
+                    tree.insert('g', 'g');
+                    tree.insert('a', 'a');
+                    tree.insert('d', 'd');
+                    tree.insert('i', 'i');
+                    tree.insert('c', 'c');
+                    tree.insert('e', 'e');
+                    tree.insert('h', 'h');
+
+                    assertMany(['f','b','a','d','c','e','g','i','h'], tree.traversePreOrder());
+                });
+            });
+
+            describe("traverseInOrder()", function() {
+                it("can correctly traverse binary search tree in-order", function() {
+                    tree.insert('f', 'f');
+                    tree.insert('b', 'b');
+                    tree.insert('g', 'g');
+                    tree.insert('a', 'a');
+                    tree.insert('d', 'd');
+                    tree.insert('i', 'i');
+                    tree.insert('c', 'c');
+                    tree.insert('e', 'e');
+                    tree.insert('h', 'h');
+
+                    assertMany(['a','b','c','d','e','f','g','h','i'], tree.traverseInOrder());
+                });
+            });
+
+            describe("traversePostOrder()", function() {
+                it("can correctly traverse binary search tree post-order", function() {
+                    tree.insert('f', 'f');
+                    tree.insert('b', 'b');
+                    tree.insert('g', 'g');
+                    tree.insert('a', 'a');
+                    tree.insert('d', 'd');
+                    tree.insert('i', 'i');
+                    tree.insert('c', 'c');
+                    tree.insert('e', 'e');
+                    tree.insert('h', 'h');
+
+                    assertMany(['a','c','e','d','b','h','i','g','f'], tree.traversePostOrder());
+                });
+            });
+
+            describe("traverseLevelOrder()", function() {
+                it("can correctly traverse binary search tree level-order", function() {
+                    tree.insert('f', 'f');
+                    tree.insert('b', 'b');
+                    tree.insert('g', 'g');
+                    tree.insert('a', 'a');
+                    tree.insert('d', 'd');
+                    tree.insert('i', 'i');
+                    tree.insert('c', 'c');
+                    tree.insert('e', 'e');
+                    tree.insert('h', 'h');
+
+                    assertMany(['f','b','g','a','d','i','c','e','h'], tree.traverseLevelOrder());
+                });
+            });
+        });
+    };
+
+    return BinarySearchTreeTestRunner;
+})();
+
+var testRunner = new BinarySearchTreeTestRunner();
+testRunner.runTests();
